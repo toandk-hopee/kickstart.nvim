a9e2ff53414241a65f31cbe931283d47ba772392
s3694679 <s3694679@rmit.edu.vn>
2024-08-04 16:01:27 +0700

    idk

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,14 +1,4 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
 return {
-  --  {
-  --    'lambdalisue/vim-kensaku-search',
-  --    cmd = 'VKen',
-  --    keys = { { 'vkensaku', ':cnoremap <cr>(kensaku-search-replace)<cr>', desc = 'Japanese search with romaji' } },
-  --    opts = {},
-  --  },
   {
     'kdheepak/lazygit.nvim',
     cmd = {
@@ -18,12 +8,9 @@ return {
       'LazyGitFilter',
       'LazyGitFilterCurrentFile',
     },
-    -- optional for floating window border decoration
     dependencies = {
       'nvim-lua/plenary.nvim',
     },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
     keys = {
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
