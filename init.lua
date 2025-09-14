e89688c370ad41a1d737cdd02a73a3053200e3e6
noatdk <toanduong8199@gmail.com>
2025-09-15 00:30:44 +0700

    uncomment relative line number

--- init.lua
+++ init.lua
@@ -58,7 +58,7 @@ end
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -577,7 +577,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        --tsserver = {},
+        tsserver = {},
         --
 
         lua_ls = {
