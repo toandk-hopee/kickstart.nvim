a356f422a67656f61c0e2209e4bcf4becfcea3dd
noatdk <toanduong8199@gmail.com>
2025-09-15 00:30:51 +0700

    auto-session

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -15,4 +15,16 @@ return {
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
   },
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+    dependencies = {
+      'nvim-telescope/telescope.nvim', -- Only needed if you want to use sesssion lens
+    },
+    config = function()
+      require('auto-session').setup {
+        auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      }
+    end,
+  },
 }
