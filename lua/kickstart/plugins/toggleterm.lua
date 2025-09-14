25343ad8199135438f60d8bcba6e1afa740dd624
noatdk <toanduong8199@gmail.com>
2025-09-15 00:30:36 +0700

    update: use vim builtin opts for cmd instead of custom dotfile

--- lua/kickstart/plugins/toggleterm.lua
+++ lua/kickstart/plugins/toggleterm.lua
@@ -2,29 +2,42 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   config = function()
+    local run_term_id = 99
+    local fidget = require 'fidget'
+    local toggleterm = require 'toggleterm.terminal'
     require('toggleterm').setup {
       lazy = true,
       open_mapping = { [[<c-t>]], [[<c-¥>]] },
+      on_stdout = function(t, job, data, name)
+        local efm = vim.opt.errorformat._value
+        if t.id == run_term_id then
+          -- new array
+          local new_data = {}
+          for i, line in pairs(data) do
+            if line ~= '' then
+              -- new_data[#new_data + 1] = line:gsub('\27%[[%d;]*[mK]', '')
+              new_data[#new_data + 1] = line
+            end
+          end
+          vim.fn.setqflist({}, ' ', { lines = new_data, efm = efm })
+        end
+      end,
     }
     vim.keymap.set('n', '<leader>tr', function()
-      local fidget = require 'fidget'
-
       -- find ".ttermrun" in the working directory for cmd to run
-      local cwd = vim.fn.getcwd()
-      local ttermrun = cwd .. '/.ttermrun'
-      local cmd = ''
-      if vim.fn.filereadable(ttermrun) == 1 then
-        cmd = vim.fn.readfile(ttermrun)[1]
-      else
-        fidget.notify('No run config is found in the current directory', vim.log.levels.WARN)
-        return
+      -- local cwd = vim.fn.getcwd()
+      local cmd = vim.opt.makeprg._value
+      if cmd == 'make' then
+        -- if vim.fn.filereadable(ttermrun) == 1 then
+        --   cmd = vim.fn.readfile(ttermrun)[1]
+        -- else
+        fidget.notify('No run config is found (default makeprg)', vim.log.levels.WARN)
       end
 
       -- tries to find toggleterm with id 99 (arbitrarily high number)
       -- if it exists, sends SIGINT/2 (interrupt) to the first child process
       -- if not, creates a new terminal with the same id
-      local toggleterm = require 'toggleterm.terminal'
-      local term = toggleterm.get(99, true)
+      local term = toggleterm.get(run_term_id, true)
       if term ~= nil then
         local jobpid = vim.fn.jobpid(term.job_id)
         local termchildren = vim.api.nvim_get_proc_children(jobpid)
@@ -32,8 +45,9 @@ return {
           vim.system { 'kill', '-2', '-' .. termchildren[1] }
         end
       else
-        term = toggleterm.Terminal:new { id = 99, display_name = 'RUN' }
+        term = toggleterm.Terminal:new { id = run_term_id, display_name = 'RUN' }
         term:spawn()
+        term:open()
       end
       if term ~= nil then
         fidget.notify('Running: ' .. cmd, vim.log.levels.INFO)
